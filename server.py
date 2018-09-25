84b8a0f3591a07d802a53bde82c11868ecc5e302
Jonathan <jonathandeon.k@gmail.com>
2018-09-25 12:36:39 +0530

    [task]: added cors

--- server.py
+++ server.py
@@ -1,15 +1,19 @@
 from evaluate import parse_input_story, getAnswer, get_ran_task, get_vocab, load_babi_task
 from flask import Flask, request,jsonify
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
 print(app)
 
-   
+cors = CORS(app, resources={r"/*": {"origins": "*"}})
+
 @app.route('/', methods=['GET'])
+@cross_origin(origin='*')
 def index():
     return "Hello, World!"
 
 @app.route('/sample', methods=['GET'])
+@cross_origin(origin='*')
 def task():
     task = request.args.get('task')
     print("Server task ID : " ,task)
@@ -18,6 +22,7 @@ def task():
    
 
 @app.route('/vocab', methods=['GET'])
+@cross_origin(origin='*')
 def vocab():
     task = request.args.get('task')
     load_babi_task(task)
@@ -26,6 +31,7 @@ def vocab():
 
 
 @app.route('/post', methods=['POST'])
+@cross_origin(origin='*')
 def post_route():
     if request.method == 'POST':
         
